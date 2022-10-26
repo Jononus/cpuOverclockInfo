8ec49034fb30a2288759b0d518088d93b5bdeaf1
Jonas Hübner <jonas.97.huebner@fau.de>
2022-10-26 17:50:25 +0200

    Added CPU Min, Max and Avg Temp

--- cpuTem.sh
+++ cpuTem.sh
@@ -9,8 +9,15 @@
 #			sudo apt-get install lm-sensors (for Ubuntu)
 #		  and run:
 #			sudo sensors-detect
-#		  scan everything you need or just type "YES" every time :) 
+#		  scan everything you need or just type "YES" every time the scan asks:) 
 
+minTemp=200
+maxTemp=0
+avgTemp=0
+gesTemp=0
+numTemp=0
+minTakt=0
+maxTakt=0
 for i in {1..100000}
 do
 	clear
@@ -19,9 +26,34 @@ do
 	echo 
 	echo
 	echo
-	sensors | grep --color CPU
-	sensors | grep --color MB
+	#Calculate min and max temperature
+	heat=$(sensors | grep --color "CPU Temperature")
+	#echo $heat
+	temp=${heat:21:2} #bei Laenge 4 bekommt man Kommazahl
+	#echo $temp
+	if [ $temp -lt $minTemp ]
+	then
+		minTemp=$temp
+	fi
+	if [ $temp -gt $maxTemp ]
+	then
+		maxTemp=$temp
+	fi
+	gesTemp=$(expr $gesTemp + $temp)
+	numTemp=$(expr $numTemp + 1)
+	avgTemp=$(expr $gesTemp / $numTemp)
+	#echo "G: $gesTemp und Num: $numTemp mach AVG: $avgTemp"
+
+	#print data
+	sensors | grep --color "CPU Fan Speed"
+	sensors | grep --color "CPU Temperature"
+	sensors | grep --color "MB Temperature"
 	sensors | grep --color 'Vcore Voltage'
 	cat /proc/cpuinfo | grep --color 'cpu MHz'
+	echo "-------------------------------------------------------------"
+	echo "CPU Min: $minTemp"
+	echo "CPU Max: $maxTemp"
+	echo "CPU AVG: $avgTemp"
+
 	sleep 2
 done
